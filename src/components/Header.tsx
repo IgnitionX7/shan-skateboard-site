bb01734e831bff834713d971aecd605f3253fb2f
Shan Amin <robin_zill@yahoo.com>
2025-04-19 16:15:16 +0500

    skateboard grid complete

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import { ButtonLink } from "./ButtonLink";
 import { Logo } from "./Logo";
+import { createClient } from "@/prismicio";
+import { PrismicNextLink } from "@prismicio/next";
 
-export function Header() {
+export async function Header() {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
   return (
     <header className="header absolute left-0 right-0 top-0 z-50 h-32 sm:h-36 md:h-40 lg:h-48 px-4 sm:px-5 md:px-6 py-4 sm:py-5 md:py-6">
       <div className="mx-auto grid w-full max-w-6xl grid-cols-[auto,auto] items-center gap-6 md:grid-cols-3">
@@ -14,9 +18,16 @@ export function Header() {
           aria-label="Main"
         >
           <ul className="flex flex-wrap items-center justify-center gap-8">
-            <li>Boards</li>
-            <li>Boards</li>
-            <li>Boards</li>
+            {settings.data.navigation.map((item) => (
+              <li key={item.link.text}>
+                <PrismicNextLink
+                  field={item.link}
+                  className="text-lg md:text-xl hover:text-brand-purple"
+                >
+                  {item.link.text}
+                </PrismicNextLink>
+              </li>
+            ))}
           </ul>
         </nav>
         <div className="justify-self-end">
