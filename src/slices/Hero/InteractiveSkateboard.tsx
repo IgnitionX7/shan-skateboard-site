2bd2642a548ebd78b502f03598f0ca74ac99a4ed
Shan Amin <robin_zill@yahoo.com>
2025-05-02 19:59:53 +0500

    Skateboard model done

---
+++ src/slices/Hero/InteractiveSkateboard.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { Skateboard } from "@/components/Skateboard";
+import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+
+type Props = {
+  deckTextureURL: string;
+  wheelTextureURL: string;
+  truckColor: string;
+  boltColor: string;
+};
+export function InteractiveSkateboard({
+  deckTextureURL,
+  wheelTextureURL,
+  truckColor,
+  boltColor,
+}: Props) {
+  return (
+    <div className="absolute inset-0 z-10 flex items-center justify-center">
+      <Canvas
+        className="min-h-[60rem] w-full"
+        camera={{ position: [1.5, 1, 1.4], fov: 55 }}
+      >
+        <Suspense>
+          <Scene
+            deckTextureURL={deckTextureURL}
+            wheelTextureURL={wheelTextureURL}
+            truckColor={truckColor}
+            boltColor={boltColor}
+          />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
+
+function Scene({
+  deckTextureURL,
+  wheelTextureURL,
+  truckColor,
+  boltColor,
+}: Props) {
+  return (
+    <group>
+      {/* <pointLight position={[1, 1, 1]} intensity={5} />
+      <pointLight position={[-2, 1, 1]} intensity={5} /> */}
+      <OrbitControls />
+      <Environment files={"/hdr/warehouse-256.hdr"} />
+      {/* <mesh>
+        <meshStandardMaterial />
+        <boxGeometry />
+      </mesh> */}
+      <Skateboard
+        wheelTextureURLs={[wheelTextureURL]}
+        wheelTextureURL={wheelTextureURL}
+        deckTextureURLs={[deckTextureURL]}
+        deckTextureURL={deckTextureURL}
+        truckColor={truckColor}
+        boltColor={boltColor}
+        constantWheelSpin
+      />
+      <ContactShadows opacity={0.6} position={[0, -0.08, 0]} />
+    </group>
+  );
+}
