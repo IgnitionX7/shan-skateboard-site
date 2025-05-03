48e1700330d641a104013a40fb9d8e0446c59b72
Shan Amin <robin_zill@yahoo.com>
2025-05-03 14:17:26 +0500

    Hotspots done

--- src/slices/Hero/InteractiveSkateboard.tsx
+++ src/slices/Hero/InteractiveSkateboard.tsx
@@ -4,8 +4,9 @@ import * as THREE from "three"; // import everything from "three" and name it as
 import { Skateboard } from "@/components/Skateboard";
 import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
 import { Canvas, ThreeEvent } from "@react-three/fiber";
-import { Suspense, useRef } from "react";
+import { Suspense, useRef, useState } from "react";
 import gsap from "gsap";
+import Hotspot from "./Hotspot";
 
 type Props = {
   deckTextureURL: string;
@@ -47,14 +48,24 @@ function Scene({
   const containerRef = useRef<THREE.Group>(null);
   const originRef = useRef<THREE.Group>(null);
 
+  const [animating, setAnimating] = useState(false);
+  const [showHotspot, setShowHotspot] = useState({
+    front: true,
+    middle: true,
+    back: true,
+  });
+
   function onClick(event: ThreeEvent<MouseEvent>) {
     event.stopPropagation();
     const board = containerRef.current;
     const origin = originRef.current;
 
-    if (!board || !origin) return;
+    if (!board || !origin || animating) return;
 
     const { name } = event.object;
+
+    // When you want to use a variable as property name inside an object literal, you need to wrap it in square brackets e.g [name] in this case
+    setShowHotspot((current) => ({ ...current, [name]: false }));
 
     if (name === "back") {
       ollie(board);
@@ -114,8 +125,9 @@ function Scene({
   }
 
   function jumpBoard(board: THREE.Group) {
+    setAnimating(true);
     gsap
-      .timeline()
+      .timeline({ onComplete: () => setAnimating(false) })
       .to(board.position, {
         y: 0.8,
         duration: 0.51,
@@ -151,14 +163,35 @@ function Scene({
               boltColor={boltColor}
               constantWheelSpin
             />
+
+            <Hotspot
+              isVisible={!animating && showHotspot.front}
+              position={[0, 0.38, 1]}
+              color="#B8FC39"
+            />
+
             <mesh position={[0, 0.27, 0.9]} name="front" onClick={onClick}>
               <boxGeometry args={[0.6, 0.2, 0.58]} />
               <meshStandardMaterial visible={false} />
             </mesh>
+
+            <Hotspot
+              isVisible={!animating && showHotspot.middle}
+              position={[0, 0.33, 0]}
+              color="#FF7A51"
+            />
+
             <mesh position={[0, 0.27, 0]} name="middle" onClick={onClick}>
               <boxGeometry args={[0.6, 0.1, 1.2]} />
               <meshStandardMaterial visible={false} />
             </mesh>
+
+            <Hotspot
+              isVisible={!animating && showHotspot.back}
+              position={[0, 0.35, -0.9]}
+              color="#46ACFA"
+            />
+
             <mesh position={[0, 0.27, -0.9]} name="back" onClick={onClick}>
               <boxGeometry args={[0.6, 0.2, 0.58]} />
               <meshStandardMaterial visible={false} />
